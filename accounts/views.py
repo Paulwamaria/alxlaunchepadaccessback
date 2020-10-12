04028369c16f1420fd816164bb2f5226561cb610
Paul Wamaria <paulwamaria@gmail.com>
2020-10-12 13:00:34 +0300

    Fix doctstring and unused import issue

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    '''
-    This view will expose the homepage endpoint.
-    '''
+    '''This view will expose the homepage endpoint.'''
     title = "Alpdaccessor"
     greet = "Welcome to Alxlpdaccessor API home page."
     context = {
--- alpdaccess/asgi.py
+++ alpdaccess/asgi.py
@@ -1,5 +1,4 @@
-"""
-ASGI config for alpdaccess project.
+"""ASGI config for alpdaccess project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
