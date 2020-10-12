c8966eecb871f4712a51c97764eb8aeb3df91302
Paul Wamaria <paulwamaria@gmail.com>
2020-10-12 13:14:34 +0300

    Fix doctstring issue

--- alpdaccess/asgi.py
+++ alpdaccess/asgi.py
@@ -1,9 +1,11 @@
-"""ASGI config for alpdaccess project.
+"""
+ASGI config for alpdaccess project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+
 """
 
 import os
