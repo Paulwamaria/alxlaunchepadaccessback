12fe6ed37ffe938e42ed47de3ec210fe9f5cc693
Paul Wamaria <paulwamaria@gmail.com>
2020-10-12 14:32:38 +0300

    Fix docstring issue

--- alpdaccess/urls.py
+++ alpdaccess/urls.py
@@ -1,4 +1,6 @@
-"""alpdaccess URL Configuration.
+"""
+
+alpdaccess URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
