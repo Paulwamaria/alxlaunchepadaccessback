de9aa4fa89f09669c9c5cfbbf44fc526d676eb9c
Paul Wamaria <paulwamaria@gmail.com>
2020-10-12 12:31:55 +0300

    Initial file structure

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn alpdaccess.wsgi --log-file -
---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    '''
+    This view will expose the homepage endpoint.
+    '''
+    title = "Alpdaccessor"
+    greet = "Welcome to Alxlpdaccessor API home page."
+    context = {
+        "title":title,
+        "greet":greet,
+    }
+    return render(request,'accounts/index.html',context)
