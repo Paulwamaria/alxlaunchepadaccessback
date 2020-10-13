86c53e48232d2beda032ed589188bf68fb07a362
Paul Wamaria <paulwamaria@gmail.com>
2020-10-13 15:56:18 +0300

    Fix docstring issue

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
+
 class UserAccountManager(BaseUserManager):
+    
     """Returns a new user after legit registration credentials are provided.
 
     Args:
@@ -10,21 +12,24 @@ class UserAccountManager(BaseUserManager):
     Returns:
         User: returns and saves a user object in the database.
     """
+
     def create_user(self, email, firstName, lastName, password=None):
         if not email:
             raise ValueError("Users must have a valid email address")
-        
+
         email = self.normalize_email(email)
-        user = self.model(email=email, firstName = firstName, lastName=lastName )
-        
+        user = self.model(email=email, firstName=firstName, lastName=lastName)
+
         user.set_password(password)
         user.save()
         return user
-    
-    def create_superuser():
-        pass
-    
+
+    # def create_superuser():
+    #     pass
+
+
 class UserAccount(AbstractBaseUser, PermissionsMixin):
+    
     """Validates registration data and instantiates the UserAccountManager
 
     Args:
@@ -39,11 +44,17 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     lastName = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
-    
+
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['firstName', 'lastName']
 
     def __str__(self):
+        
+        """Returns the object representation.
+
+        Returns:
+            email: Returns the email address of the requested user object.
+        """
         return self.email
